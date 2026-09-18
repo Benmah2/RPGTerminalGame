c566961c5291d4a88cc0de92f17c89286da3e19b
Benmah2 <benmahoo2@gmail.com>
2026-09-18 10:39:43 +0200

    Woodcutting skill with XP and chop training loop

---
+++ RpgConsoleGame/Game.cs
@@ -0,0 +1,57 @@
+﻿using RpgConsoleGame;
+var chop = "chop";
+var woodCutting = "WoodCutting";
+var chosenSkill = chooseSkill();
+
+
+string chooseSkill()
+{
+    var userName = "";
+
+    while (userName != woodCutting) {
+        Console.WriteLine("Which skill are you going to train? ");
+        userName = Console.ReadLine();
+        if (userName != woodCutting)
+        {
+            Console.WriteLine("No. A skill! Try again!");
+        } 
+
+    } return userName;
+}
+
+void trainingSkill(Skill p)
+{
+
+
+    while (chop == "chop")
+    {
+        Console.WriteLine("WoodCutting. Write 'chop' to level up the skill.");
+        chop = Console.ReadLine();
+
+        if ( chop == "chop") {
+              p.AddXP(1);
+
+            Console.WriteLine("______________");
+            Console.WriteLine("Skill: " + p.Name);
+            Console.WriteLine("Exp: " + p.XP);
+            Console.WriteLine("______________");
+            Console.WriteLine("");
+            Console.WriteLine("1 exp. Keep going!");
+            Console.WriteLine("");
+        }
+        else
+        {
+            Console.WriteLine("Ok stopped.");
+        }
+
+    }
+}
+
+
+var p = new Skill(chosenSkill, 0);
+trainingSkill(p);
+p.AddXP(1);
+
+
+
+Console.WriteLine("Cool");
---
+++ RpgConsoleGame/Skill.cs
@@ -0,0 +1,20 @@
+﻿namespace RpgConsoleGame
+{
+    public class Skill
+    {
+        public string Name { get; private set; }
+
+        public int XP { get; private set; }
+
+        public Skill(string name, int xp)
+        {
+            Name = name;
+            XP = xp;
+        }
+        public int AddXP(int amount)
+        {
+            XP += amount;
+            return XP;
+        }
+    }
+}
